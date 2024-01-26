4cde4f8860a0d6cfc8a1c6eb81822145df2dab76
Mateusz Milczarek <milczary.mm@gmail.com>
2024-01-26 21:13:51 +0100

    Fix

--- Tables/service.sql
+++ Tables/service.sql
@@ -3,7 +3,7 @@ CREATE or replace TYPE TasksArray_type force AS VARRAY(10) OF TASK_TYPE;
 CREATE or replace TYPE Service_type force AS OBJECT (
     ServiceID NUMBER,
     tasks TasksArray_type,
-    employee REF Employees,
+    employee REF EMPLOYEE_TYPE,
     owner REF OWNER_TYPE,
     car REF CAR_TYPE,
     position NUMBER,
