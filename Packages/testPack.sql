3a751e95b74e4187f61a174fc4816f81ecf51445
Lecrut <filip.lecrut12@gmail.com>
2024-01-27 13:24:44 +0100

    add test pack

--- Packages/testPack.sql
+++ Packages/testPack.sql
@@ -5,3 +5,39 @@ SELECT * FROM USER_TYPES;
 SELECT * FROM USER_OBJECTS;
 
 DROP TABLE SERVICETABLE
+
+
+select * from CARTABLE;
+drop table CARTABLE;
+commit;
+drop table SERVICETABLE;
+delete from CARTABLE;
+commit;
+
+begin
+    OWNERPACKAGE.ShowAllOwners();
+end;
+
+begin
+    TASKSPACKAGE.SHOWTASKS();
+end;
+
+begin
+    EMPLOYEESPACKAGE.SHOWEMPLOYEESLIST();
+end;
+
+begin
+    CARPACKAGE.SHOWCARS();
+end;
+
+
+DECLARE
+    v_Brand VARCHAR2(50) := 'Toyota';
+    v_Model VARCHAR2(50) := 'Corolla';
+    v_Year_of_production NUMBER := 2010;
+    v_Registration_number VARCHAR2(15) := 'ABC 123';
+    v_Mileage NUMBER := 2300;
+    v_VIN VARCHAR2(20) := '1234567890';
+BEGIN
+    CARPACKAGE.AddCar(v_Brand, v_Model, v_Year_of_production, v_Registration_number, v_Mileage, v_VIN);
+END;
