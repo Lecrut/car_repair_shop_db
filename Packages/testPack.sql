881c2d96d3737325967cb6ac8284d3f8ef7c8d8e
Mateusz Milczarek <milczary.mm@gmail.com>
2024-01-29 11:06:13 +0100

    Test pack

--- Packages/testPack.sql
+++ Packages/testPack.sql
@@ -1,62 +1,22 @@
-SELECT * FROM USER_TABLES;
-
-SELECT * FROM USER_TYPES;
-
-SELECT * FROM USER_OBJECTS;
-
-DROP TABLE SERVICETABLE;
-
-
-select * from CARTABLE;
-drop table CARTABLE;
-commit;
-drop table SERVICETABLE;
-delete  from SERVICETABLE;
+delete from SERVICETABLE;
 delete from CARTABLE;
-commit;
+delete from CLIENTTABLE;
+delete from EMPLOYEESTABLE;
+
+
+
 select * from WORKSHOPTABLE;
-select * from CLIENTTABLE;
-begin
-    OWNERPACKAGE.ShowAllOwners();
-    OWNERPACKAGE.SHOWOWNERBYPHONE(123456789);
-end;
+select * from TASKSTABLE;
 
-DECLARE
-  client_ref REF OWNER_TYPE;
-  client_obj OWNER_TYPE;
-BEGIN
-  -- Wywołujemy funkcję z numerem telefonu
-  client_ref := OWNERPACKAGE.GETOWNERREFBYPHONE('123456789');
-  -- Jeśli funkcja zwróciła referencję, wyłuskujemy obiekt z referencji
-  IF client_ref IS NOT NULL THEN
-      DBMS_OUTPUT.PUT_LINE('success');
---     client_obj := DEREF(client_ref);
-    SELECT DEREF(client_ref) INTO client_obj FROM DUAL;
-    -- Wypisujemy atrybuty obiektu
-    dbms_output.put_line('OwnerID: ' || client_obj.OwnerID || ', Name: ' || client_obj.Name || ', Surname: ' || client_obj.Surname || ', Phone: ' || client_obj.Phone);
-  ELSE
-    -- Jeśli funkcja zwróciła NULL, wypisujemy komunikat
-    dbms_output.put_line('No clients founded');
-  END IF;
-END;
-
+commit;
 
 begin
-    TASKSPACKAGE.SHOWTASKS();
-end;
-
-begin
-    SERVICEPACKAGE.PRINTSERVICEDATABYDATE(TO_DATE('2024-03-01', 'YYYY-MM-DD'));
+    EMPLOYEESPACKAGE.ADDEMPLOYEE('Jan', 'Kowalski', 2000, 'Majster', TO_DATE('2024-01-01', 'YYYY-MM-DD'));
 end;
 
 begin
     EMPLOYEESPACKAGE.SHOWEMPLOYEESLIST();
 end;
-
-begin
-    CARPACKAGE.SHOWCARS();
-end;
-
 
 DECLARE
     v_Brand VARCHAR2(50) := 'Toyota';
@@ -64,26 +24,37 @@ DECLARE
     v_Year_of_production NUMBER := 2010;
     v_Registration_number VARCHAR2(15) := 'ABC 123';
     v_Mileage NUMBER := 2300;
-    v_VIN VARCHAR2(20) := '1234567890';
+    v_VIN VARCHAR2(20) := 'ABC1234';
 BEGIN
     CARPACKAGE.AddCar(v_Brand, v_Model, v_Year_of_production, v_Registration_number, v_Mileage, v_VIN);
 END;
 
 begin
-    SERVICEPACKAGE.PRINTFREEHOURS(TO_DATE('2024-03-01', 'YYYY-MM-DD'));
+    CARPACKAGE.SHOWCARS();
 end;
 
+begin
+    OWNERPACKAGE.ADDOWNER('Zygfryd', 'Nowak', '503976322');
+end;
 
-DECLARE
-  tasks TasksArray_type;
+begin
+    OWNERPACKAGE.SHOWALLOWNERS();
+    OWNERPACKAGE.SHOWOWNERBYPHONE('blad');
+    OWNERPACKAGE.SHOWOWNERBYPHONE('503976322');
+end;
+
+begin
+    SERVICEPACKAGE.PRINTFREEHOURS( TO_DATE('2024-03-01', 'YYYY-MM-DD'));
+end;
+
+begin
+    SERVICEPACKAGE.PRINTSERVICEDATABYDATE(TO_DATE('2024-03-01', 'YYYY-MM-DD'));
+end;
+
 BEGIN
---     tasks := TASKSPACKAGE.CREATETASKSARRAY(SYS.ODCINUMBERLIST(5, 4 ,3 ,2 ,1));
-  -- Wypisujemy elementy tablicy
-
-    SERVICEPACKAGE.ADDSERVICE('1234567890', 1, '123456789', TO_DATE('2024-03-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), SYS.ODCINUMBERLIST(1), 2);
-
---   TASKSPACKAGE.SHOWTASKS();
+    SERVICEPACKAGE.ADDSERVICE('ABC1234', 63, '503976322', TO_DATE('2024-03-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), SYS.ODCINUMBERLIST(1), 2);
 END;
 
-select TASKS, deref(EMPLOYEE), deref(OWNER), POSITION, HOUR  from SERVICETABLE;
-select * from SERVICETABLE;
+begin
+    SERVICEPACKAGE.PRINTCLIENTHISTORY('123456789');
+end;
