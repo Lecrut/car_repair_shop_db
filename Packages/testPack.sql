ddb97e14d06a4cee8336643ef10ad1c7dede613d
Mateusz Milczarek <milczary.mm@gmail.com>
2024-02-01 14:30:58 +0100

    updated testPack.sql

--- Packages/testPack.sql
+++ Packages/testPack.sql
@@ -11,7 +11,7 @@ select * from TASKSTABLE;
 commit;
 
 begin
-    EMPLOYEESPACKAGE.ADDEMPLOYEE('Jan', 'Kowalski', 2000, 'Majster', TO_DATE('2024-01-01', 'YYYY-MM-DD'));
+    EMPLOYEESPACKAGE.ADDEMPLOYEE('Jan1', 'Kowalski2', 2000, 'Majster', TO_DATE('2024-01-01', 'YYYY-MM-DD'));
 end;
 
 begin
@@ -19,12 +19,12 @@ begin
 end;
 
 DECLARE
-    v_Brand VARCHAR2(50) := 'Toyota';
-    v_Model VARCHAR2(50) := 'Corolla';
+    v_Brand VARCHAR2(50) := 'Seat';
+    v_Model VARCHAR2(50) := 'Ibiza';
     v_Year_of_production NUMBER := 2010;
-    v_Registration_number VARCHAR2(15) := 'ABC 123';
+    v_Registration_number VARCHAR2(15) := 'ABC 1333';
     v_Mileage NUMBER := 2300;
-    v_VIN VARCHAR2(20) := 'ABC1234';
+    v_VIN VARCHAR2(20) := '3254323';
 BEGIN
     CARPACKAGE.AddCar(v_Brand, v_Model, v_Year_of_production, v_Registration_number, v_Mileage, v_VIN);
 END;
@@ -34,12 +34,12 @@ begin
 end;
 
 begin
-    OWNERPACKAGE.ADDOWNER('Zygfryd', 'Nowak', '503976322');
+    OWNERPACKAGE.ADDOWNER('Jan', 'Nowacki', '503127642');
 end;
 
 begin
-    OWNERPACKAGE.SHOWALLOWNERS();
-    OWNERPACKAGE.SHOWOWNERBYPHONE('blad');
+--     OWNERPACKAGE.SHOWALLOWNERS();
+--     OWNERPACKAGE.SHOWOWNERBYPHONE('blad');
     OWNERPACKAGE.SHOWOWNERBYPHONE('503976322');
 end;
 
@@ -47,14 +47,15 @@ begin
     SERVICEPACKAGE.PRINTFREEHOURS( TO_DATE('2024-03-01', 'YYYY-MM-DD'));
 end;
 
+
 begin
     SERVICEPACKAGE.PRINTSERVICEDATABYDATE(TO_DATE('2024-03-01', 'YYYY-MM-DD'));
 end;
 
 BEGIN
-    SERVICEPACKAGE.ADDSERVICE('ABC1234', 63, '503976322', TO_DATE('2024-03-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), SYS.ODCINUMBERLIST(1), 2);
+    SERVICEPACKAGE.ADDSERVICE('ABC1234', 3, '503976322', TO_DATE('2024-03-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), SYS.ODCINUMBERLIST(1), 2);
 END;
 
 begin
-    SERVICEPACKAGE.PRINTCLIENTHISTORY('123456789');
+    SERVICEPACKAGE.PRINTCLIENTHISTORY('503976322');
 end;
